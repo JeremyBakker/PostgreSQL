dfdef382deb8f590bfd3c843d8b3499fe20fa487
JeremyBakker <jeremy.t.bakker@gmail.com>
2017-07-09 14:45:16 -0500

    more practice

--- PostreSQL.sql
+++ PostreSQL.sql
@@ -151,3 +151,24 @@ SELECT rating, AVG(rental_duration)
 FROM film
 GROUP BY rating
 HAVING AVG(rental_duration) > 5;
+
+SELECT customer_id, SUM(AMOUNT)
+FROM payment
+WHERE staff_id = 2 and SUM(AMOUNT) >= 110;
+
+SELECT customer_id, SUM(AMOUNT)
+FROM payment
+WHERE staff_id = 2
+GROUP BY customer_id
+HAVING SUM(AMOUNT) >= 110;
+
+SELECT COUNT(*)
+FROM film
+WHERE title
+LIKE 'J%'
+
+SELECT first_name, last_name
+FROM customer
+WHERE first_name LIKE 'E%' AND address_id < 500
+ORDER BY customer_id DESC
+LIMIT 1
